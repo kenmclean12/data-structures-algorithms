ae5ada4a0839f6b23d25625082a53cde2c175453
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-04-04 09:24:55 -0700

    Add: two-integer-sum - submission-20

---
+++ Data Structures & Algorithms/two-integer-sum/submission-20.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+      Dictionary<int, int> map = new();
+      for (int i = 0; i < nums.Length; i++) {
+        int needed = target - nums[i];
+        if (map.ContainsKey(needed)) {
+            return new int[] { map[needed], i };
+        }
+
+        map[nums[i]] = i;
+      }
+
+      return Array.Empty<int>();
+    }
+}
