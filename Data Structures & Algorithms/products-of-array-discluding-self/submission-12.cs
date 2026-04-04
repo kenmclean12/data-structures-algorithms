d017b3e160a07652aba689b582a5b0ecac9d6f42
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-04-04 09:51:42 -0700

    Add: products-of-array-discluding-self - submission-12

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-12.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int[] result = new int[nums.Length];
+
+        int leftProduct = 1;
+        for (int i = 0; i < nums.Length; i++) {
+            result[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        int rightProduct = 1;
+        for (int i = nums.Length - 1; i >= 0; i--) {
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
+    }
+}
