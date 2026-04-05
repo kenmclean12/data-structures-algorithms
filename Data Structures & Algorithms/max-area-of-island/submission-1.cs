11a7aabb099c8fef5ace39e0b8596016a8818e9a
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-04-04 17:55:21 -0700

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int MaxAreaOfIsland(int[][] grid) {
+        int max = 0;
+
+        for (int r = 0; r < grid.Length; r++) {
+            for (int c = 0; c < grid[0].Length; c++) {
+                if (grid[r][c] == 1) {
+                    max = Math.Max(max, Dfs(grid, r, c));
+                }
+            }
+        }
+
+        return max;
+    }
+
+    private int Dfs(int[][] grid, int r, int c) {
+        if (
+            r < 0 ||
+            c < 0 ||
+            r >= grid.Length ||
+            c >= grid[0].Length ||
+            grid[r][c] == 0
+        ) {
+            return 0;
+        }
+
+        grid[r][c] = 0;
+        int area = 1;
+
+        area += Dfs(grid, r + 1, c);
+        area += Dfs(grid, r - 1, c);
+        area += Dfs(grid, r, c + 1);
+        area += Dfs(grid, r, c - 1);
+
+        return area;
+    }
+}
