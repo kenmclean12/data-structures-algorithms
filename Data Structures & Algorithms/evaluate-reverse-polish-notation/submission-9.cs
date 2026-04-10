cb90bd2b42234ab8730346bb4c7cc35d0a0a62ac
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-04-10 16:58:45 -0700

    Add: evaluate-reverse-polish-notation - submission-9

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-9.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        Stack<int> stack = new();
+
+        foreach (string token in tokens) {
+            if (
+                token == "+" || 
+                token == "-" ||
+                token == "/" ||
+                token == "*"
+            ) {
+                int b = stack.Pop();
+                int a = stack.Pop();
+                int result = token switch {
+                    "+" => a + b,
+                    "-" => a - b,
+                    "*" => a * b,
+                    "/" => a / b,
+                };
+
+                stack.Push(result);
+            } else {
+                stack.Push(int.Parse(token));
+            }
+        }
+
+        return stack.Pop();
+    }
+}
