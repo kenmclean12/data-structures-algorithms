a1e98aa89e59787f3157745540571e84983ee308
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-04-11 16:12:17 -0700

    Add: search-2d-matrix - submission-8

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-8.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int rows = matrix.Length;
+        int cols = matrix[0].Length;
+
+        int r = 0;
+        int c = cols - 1;
+
+        while (r < rows && c >= 0) {
+            if (matrix[r][c] == target) return true;
+            if (matrix[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+
+        return false;
+    }
+}
