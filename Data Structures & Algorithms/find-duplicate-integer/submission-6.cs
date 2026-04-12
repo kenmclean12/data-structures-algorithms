26adcfad533ececf39b59fd114ef4ac9da9f0706
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-04-12 15:19:21 -0700

    Add: find-duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-6.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int FindDuplicate(int[] nums) {
+        HashSet<int> seen = new();
+        foreach(int num in nums) {
+            if (seen.Contains(num)) return num;
+            seen.Add(num);
+        }
+        
+        return -1;
+    }
+}
