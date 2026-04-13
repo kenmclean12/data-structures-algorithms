98c70299149fd4e9a82db963e060a3522536886b
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-04-13 13:26:12 -0700

    Add: subtree-of-a-binary-tree - submission-32

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-32.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public bool IsSubtree(TreeNode root, TreeNode subRoot) {
+       if (subRoot == null) return true;
+       if (root == null) return false;
+       if (IsSameTree(root, subRoot)) return true;
+
+       var left = IsSubtree(root.left, subRoot);
+       var right = IsSubtree(root.right, subRoot);
+
+       return left || right;
+    }
+
+    private bool IsSameTree(TreeNode tree1, TreeNode tree2) {
+        if (tree1 == null && tree2 == null) return true;
+        if (tree1 == null || tree2 == null) return false;
+
+        bool leftSame = IsSameTree(tree1.left, tree2.left);
+        bool rightSame = IsSameTree(tree1.right, tree2.right);
+
+        return tree1.val == tree2.val && leftSame && rightSame;
+    }
+}
