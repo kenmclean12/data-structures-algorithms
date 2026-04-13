b389fcb7ef90815ef6ac1a8ad3e5fa159ea62007
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-04-13 12:51:50 -0700

    Add: reverse-nodes-in-k-group - submission-1

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-1.cs
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode ReverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (head != null) {
+           ListNode check = head;
+           int count = 0;
+           while (count < k && check != null) {
+              count++;
+              check = check.next;
+           }
+
+           if (count < k) {
+              tail.next = head;
+              break;
+           }
+
+           ListNode prev = null;
+           ListNode curr = head;
+           for (int i = 0; i < k; i++) {
+              var tmp = curr.next;
+              curr.next = prev;
+              prev = curr;
+              curr = tmp;
+           }
+
+           tail.next = prev;
+           tail = head;
+           head = curr;
+        }
+
+        return dummy.next;
+    }
+}
