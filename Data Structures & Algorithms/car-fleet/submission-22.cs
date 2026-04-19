dff7006bad4e23cbdf5780c7a3ef6b7e19b1c29a
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-04-19 15:37:49 -0700

    Add: car-fleet - submission-22

---
+++ Data Structures & Algorithms/car-fleet/submission-22.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int CarFleet(int target, int[] position, int[] speed) {
+       List<(int pos, double time)> list = new();
+       for (int i = 0; i < position.Length; i++) {
+         double time = (double)(target - position[i]) / speed[i];
+         list.Add((position[i], time));
+       }
+       list.Sort((a, b) => b.pos.CompareTo(a.pos));
+
+       Stack<double> stack = new();
+       for (int i = 0; i < list.Count; i++) {
+         if (stack.Count == 0 || list[i].time > stack.Peek()) {
+            stack.Push(list[i].time);
+         }
+       }
+
+       return stack.Count;
+    }
+}
