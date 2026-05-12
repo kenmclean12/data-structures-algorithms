95cec499331dfa1bd9280d377c162761e3827980
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-05-12 14:47:46 -0700

    Add: string-encode-and-decode - submission-36

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-36.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public string Encode(IList<string> strs) {
+        StringBuilder sb = new();
+
+        foreach (string str in strs) {
+            sb.Append(str.Length);
+            sb.Append('#');
+            sb.Append(str);
+        }
+
+        return sb.ToString();
+    }
+
+    public List<string> Decode(string s) {
+       List<string> result = new();
+
+       int i = 0;
+       while (i < s.Length) {
+         string lengthStr = "";
+         while (s[i] != '#') {
+            lengthStr += s[i];
+            i++;
+         }
+
+         int len = int.Parse(lengthStr);
+         i++;
+
+         string resultString = "";
+         for (int j = 0; j < len; j++) {
+            resultString += s[i];
+            i++;
+         }
+
+         result.Add(resultString);
+       }
+
+       return result;
+    }
+}
