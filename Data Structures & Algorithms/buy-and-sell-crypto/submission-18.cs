0633a9d7387a68407cc2e1ae48e809fddf61bf3b
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-05-17 08:37:47 -0700

    Add: buy-and-sell-crypto - submission-18

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-18.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        if (prices.Length == 0) return 0;
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        foreach (int price in prices) {
+            minPrice = Math.Min(minPrice, price);
+            maxProfit = Math.Max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
