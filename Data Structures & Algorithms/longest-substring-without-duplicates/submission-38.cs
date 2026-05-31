d47111ae9a31237d8242a74592f5e763e3faae4e
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-05-31 15:46:00 -0700

    Add: longest-substring-without-duplicates - submission-38

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-38.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        HashSet<char> set = new();
+        int result = 0;
+        int left = 0;
+
+        for (int i = 0; i < s.Length; i++) {
+            char c = s[i];
+            while (set.Contains(c)) {
+                set.Remove(s[left]);
+                left++;
+            }
+
+            set.Add(c);
+            int window = (i - left) + 1;
+            result = Math.Max(result, window);
+        }
+
+        return result;
+    }
+}
