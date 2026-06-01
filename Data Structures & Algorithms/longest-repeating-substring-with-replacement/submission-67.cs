6f38bf98273b8365234f344e7b8a388a158c5dd7
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-06-01 10:56:37 -0700

    Add: longest-repeating-substring-with-replacement - submission-67

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-67.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        int[] count = new int[26];
+        int left = 0;
+        int result = 0;
+
+        for (int i = 0; i < s.Length; i++) {
+          count[s[i] - 'A']++;
+          while (((i - left) + 1) - count.Max() > k) {
+            count[s[left] - 'A']--;
+            left++;
+          }
+
+          result = Math.Max(result, (i - left) + 1);
+        }
+
+        return result;
+    }
+}
+
+
