2154417aa1f71cbdf2be32818a905cbf21b623cb
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-06-01 11:09:53 -0700

    Add: minimum-window-with-characters - submission-7

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-7.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public string MinWindow(string s, string t) {
+        int[] tCount = new int[128];
+        for (int i = 0; i < t.Length; i++) {
+            tCount[t[i]]++;
+        }
+
+        int[] sCount = new int[128];
+        int left = 0;
+        string result = "";
+
+        for (int i = 0; i < s.Length; i++) {
+            sCount[s[i]]++;
+            while (CountsValid(tCount, sCount)) {
+                string totalStr = s.Substring(left, (i - left) + 1);
+                if (totalStr.Length < result.Length || result.Length == 0) {
+                    result = totalStr;
+                }
+
+                sCount[s[left]]--;
+                left++;
+            }
+        }
+
+        return result;
+    }
+
+    private bool CountsValid(int[] t, int[] s) {
+        for (int i = 0; i < 128; i++) {
+            if (t[i] > s[i]) return false;
+        }
+
+        return true;
+    }
+}
