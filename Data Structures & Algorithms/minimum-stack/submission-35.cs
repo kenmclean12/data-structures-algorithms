cd1f798e03714395b854c2d321fb90c0bee66dd9
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-06-12 08:44:01 -0700

    Add: minimum-stack - submission-35

---
+++ Data Structures & Algorithms/minimum-stack/submission-35.cs
@@ -0,0 +1,31 @@
+public class MinStack {
+    private Stack<int> stack;
+    private Stack<int> minStack;
+
+    public MinStack() {
+      stack = new();
+      minStack = new();
+    }
+    
+    public void Push(int val) {
+      stack.Push(val);
+      if (minStack.Count == 0 || val < minStack.Peek()) {
+        minStack.Push(val);
+      } else {
+        minStack.Push(minStack.Peek());
+      }
+    }
+    
+    public void Pop() {
+       stack.Pop();
+       minStack.Pop();
+    }
+    
+    public int Top() {
+       return stack.Peek();
+    }
+    
+    public int GetMin() {
+      return minStack.Peek();
+    }   
+}
