ee033a0f8241e414b37cd262e4b4c64f407a8e8a
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-06-13 13:20:06 -0700

    Add: top-k-elements-in-list - submission-63

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-63.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+       Dictionary<int, int> map = new();
+       foreach (int num in nums) {
+         if (!map.ContainsKey(num)) {
+            map[num] = 1;
+         } else {
+            map[num]++;
+         }
+       }
+
+       return map
+        .OrderByDescending(x => x.Value)
+        .Take(k)
+        .Select(x => x.Key)
+        .ToArray<int>();
+    }
+}
