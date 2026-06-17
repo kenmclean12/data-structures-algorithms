a27e77b85075a41fcdf79e45ec3003b656e396e5
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-06-17 07:50:36 -0700

    Add: trapping-rain-water - submission-52

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-52.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int Trap(int[] height) {
+       int left = 0;
+       int right = height.Length - 1;
+       int leftMax = 0;
+       int rightMax = 0;
+       int maxArea = 0;
+
+       while (left < right) {
+         if (height[left] < height[right]) {
+           if (height[left] >= leftMax) {
+             leftMax = height[left];
+           } else {
+            maxArea += (leftMax - height[left]);
+           }
+
+           left++;
+         } else {
+           if (height[right] >= rightMax) {
+             rightMax = height[right];
+           } else {
+            maxArea += (rightMax - height[right]);
+           }
+
+           right--;
+         }
+       }
+
+       return maxArea;
+    }
+}
