d90a4941e006ec7e55b81d5e8b122bfe5e4fc324
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-06-17 08:08:18 -0700

    Add: sliding-window-maximum - submission-17

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-17.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] MaxSlidingWindow(int[] nums, int k) {
+      int[] result = new int[nums.Length - k + 1];
+      for (int i = 0; i < result.Length; i++) {
+        int maxVal = int.MinValue;
+        for (int j = i; j < i + k; j++) {
+          if (nums[j] > maxVal) {
+            maxVal = nums[j];
+          }
+        }
+
+        result[i] = maxVal;
+      }
+
+      return result;
+    }
+}
