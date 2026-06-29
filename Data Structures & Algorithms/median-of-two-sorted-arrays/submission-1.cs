401dabe0d9800a7a4ec05686d6176420fd0a4abe
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-06-29 10:51:32 -0700

    Add: median-of-two-sorted-arrays - submission-1

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+        List<int> list = new();
+        foreach (int num in nums1) list.Add(num);
+        foreach (int num in nums2) list.Add(num);
+        list.Sort((a, b) => a.CompareTo(b));
+
+        int left = 0;
+        int right = list.Count - 1;
+        int mid = left + (right - left) / 2;
+        if (list.Count % 2 == 0) {
+          return ((double)(list[mid] + list[mid + 1]) / 2);
+        } else {
+          return (double)list[mid];
+        }
+    }
+}
