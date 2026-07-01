3c07e26678926dc2b020d57932960d03ed071401
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-07-01 11:43:58 -0700

    Add: eating-bananas - submission-22

---
+++ Data Structures & Algorithms/eating-bananas/submission-22.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int h) {
+      int left = 1;
+      int right = 0;
+      foreach (int pile in piles) {
+        right = Math.Max(right, pile);
+      }
+
+      while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (HoursNeeded(piles, mid) > h) {
+          left = mid + 1;
+        } else {
+          right = mid;
+        }
+      }
+
+      return left;
+    }
+
+    private int HoursNeeded(int[] piles, int k) {
+       int result = 0;
+       foreach (int pile in piles) {
+         result += (int)Math.Ceiling((double)pile / k);
+       }
+
+       return result;
+    }
+}
