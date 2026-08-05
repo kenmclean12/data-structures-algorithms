eb75444c780cfbf1363107141ab273897251eec1
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-08-05 13:45:59 -0700

    Add: two-integer-sum-ii - submission-35

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-35.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+      Dictionary<int, int> map = new();
+      for (int i = 0; i < numbers.Length; i++) {
+        int needed = target - numbers[i];
+        if (map.ContainsKey(needed)) {
+            return new int[] { map[needed], i + 1 };
+        }
+
+        map[numbers[i]] = i + 1;
+      }
+
+      return Array.Empty<int>();
+    }
+}
