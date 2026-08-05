8399df4e82b38bbb4531897ff98fad5227206c14
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-08-05 09:05:35 -0700

    Add: max-water-container - submission-50

---
+++ Data Structures & Algorithms/max-water-container/submission-50.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        int left = 0;
+        int right = heights.Length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = Math.Min(heights[left], heights[right]);
+            int w = right - left;
+            maxArea = Math.Max(maxArea, h * w);
+
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
