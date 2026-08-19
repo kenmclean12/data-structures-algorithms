9de305eec8b4f5386e1f847314770425d8f71652
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-08-19 13:26:11 -0700

    Add: valid-sudoku - submission-36

---
+++ Data Structures & Algorithms/valid-sudoku/submission-36.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        int rows = board.Length;
+        int cols = board[0].Length;
+        HashSet<string> set = new();
+
+        for (int r = 0; r < rows; r++) {
+          for (int c = 0; c < cols; c++) {
+            char val = board[r][c];
+            if (val == '.') continue;
+            if (
+              !set.Add($"{val} in row {r}") ||
+              !set.Add($"{val} in col {c}") ||
+              !set.Add($"{val} in box {r/3} - {c/3}")
+            ) {
+              return false;
+            }
+          }
+        }
+
+        return true;
+    }
+}
