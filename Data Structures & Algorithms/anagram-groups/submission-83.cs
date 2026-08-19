8888895143fe7b56230b77772b25e47f37949e6c
Ken McLean <155334404+kenmclean12@users.noreply.github.com>
2026-08-19 13:19:55 -0700

    Add: anagram-groups - submission-83

---
+++ Data Structures & Algorithms/anagram-groups/submission-83.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+       Dictionary<string, List<string>> map = new();
+       foreach (string str in strs) {
+         int[] count = new int[128];
+         for (int i = 0; i < str.Length; i++) count[str[i]]++;
+         string key = String.Join(",", count);
+
+         if (!map.ContainsKey(key)) {
+          map[key] = new List<string>();         
+         }
+
+         map[key].Add(str);
+       }
+
+       return map.Values.ToList<List<string>>();
+    }
+}
